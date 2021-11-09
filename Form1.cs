af3e020f84466cb583ae6c771714ffc7fb57eaff
Ali Can Yücel <32604562+alicanyucel@users.noreply.github.com>
2021-11-09 17:36:55 +0200

    resimokumma
    resim okuma

---
+++ Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IronOcr;
+using System.Windows.Forms;
+
+namespace ıronoct
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+          /*  var Result = new IronTesseract().Read(@"C:\ab.jpg");
+            MessageBox.Show(Result.Text);*/
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            var Result = new IronTesseract().Read(@"C:\ab.png");
+            richTextBox1.Text = Result.Text;
+        }
+    }
+}
